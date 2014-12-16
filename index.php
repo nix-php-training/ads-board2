c764844f9aaa6c5a13fa28f53792640569d0d82d
pitcorr <pitcorr@gmail.com>
2014-12-16 17:52:53 +0200

    First joint version

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include_once 'gfun.php';//help function
+
+ini_set('display_errors', 1);
+
+define("ROOT_PATH", dirname(__FILE__));
+require_once ROOT_PATH . '/framework/core/FrontController.php';
