777a75b5dd5d84afb335365b2a1dfd5f5c041b81
dimond-darrell <staskgt@gmail.com>
2014-12-19 12:07:12 +0200

    [FIX] after pull from upstream

--- framework/core/Dispatcher.php
+++ framework/core/Dispatcher.php
@@ -5,37 +5,7 @@ class Dispatcher
     private static $pureControllerName;
     private static $pureActionName;
 
-<<<<<<< HEAD
-    public static function start()
-    {
-        $controllerName = 'home';
-        $actionName = 'index';
-        self::$pureActionName = $actionName;
-        self::$pureControllerName = $controllerName;
 
-        $routes = explode('/', $_SERVER['REQUEST_URI']);
-
-        if (!empty($routes[1])) {
-            $controllerName = $routes[1];
-            self::$pureControllerName = $controllerName;
-        }
-
-        if (!empty($routes[2])) {
-            $actionName = $routes[2];
-            self::$pureActionName = $actionName;
-        }
-
-        $modelName = ucfirst(strtolower($controllerName)) . 'Model';
-        $controllerName = ucfirst(strtolower($controllerName)) . 'Controller';
-        $actionName = strtolower($actionName) . 'Action';
-
-        $modelFile = $modelName . '.php';
-        $modelPath = ROOT_PATH . '/application/models/' . $modelFile;
-        if (file_exists($modelPath)) {
-            include $modelPath;
-            Registry::set('model', $modelName);
-        }
-=======
     static function start()
     {
 
@@ -44,19 +14,13 @@ class Dispatcher
         $router->getActiveRoute();
 
         self::$pureControllerName = ucfirst($router->getControllerName());
-        self::$pureActionName  = strtolower($router->getActionName());
-
+        self::$pureActionName = strtolower($router->getActionName());
 
         $actionName = self::$pureActionName . 'Action';
         $controllerName = self::$pureControllerName . 'Controller';
->>>>>>> 31f2180af2c18b610cb490d6d41daf1639409b5c
 
         $controllerFile = $controllerName . '.php';
-<<<<<<< HEAD
         $controllerPath = ROOT_PATH . '/application/controllers/' . $controllerFile;
-=======
-        $controllerPath = "application/controllers/" . $controllerFile;
->>>>>>> 31f2180af2c18b610cb490d6d41daf1639409b5c
         if (file_exists($controllerPath)) {
             include $controllerPath;
             Registry::set('controller', $controllerName);
@@ -64,29 +28,18 @@ class Dispatcher
             self::ErrorPage404();
         }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 31f2180af2c18b610cb490d6d41daf1639409b5c
         $controller = new $controllerName(self::$pureActionName);
         $action = $actionName;
 
 
-
         if (method_exists($controller, $action)) {
             Registry::set('action', $actionName);
-
             $controller->$action();
         } else {
-<<<<<<< HEAD
-            self::ErrorPage404();
-        }
-=======
->>>>>>> 31f2180af2c18b610cb490d6d41daf1639409b5c
-
             self::ErrorPage404();
         }
     }
+
 
     private static function ErrorPage404()
     {
@@ -96,6 +49,3 @@ class Dispatcher
         header('Location:' . $host . 'error/error404');
     }
 }
-
-
-
--- index.php
+++ index.php
@@ -4,12 +4,4 @@ ini_set('display_errors', 1);
 
 define("ROOT_PATH", dirname(__FILE__));
 
-<<<<<<< HEAD
-require_once ROOT_PATH . '/framework/core/FrontController.php';
-=======
-
 include_once ROOT_PATH . '/framework/core/FrontController.php';
-
-
-
->>>>>>> 31f2180af2c18b610cb490d6d41daf1639409b5c
