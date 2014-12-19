fc0ec7f78e7d3ed0224f89dbd99a2efd79b4c67b
dimond-darrell <staskgt@gmail.com>
2014-12-19 09:24:23 +0200

    [DELETE] old view

--- framework/core/View.php
+++ framework/core/View.php
@@ -2,25 +2,6 @@
 
 class View
 {
-
-//    function generate($content, $layout = 'layout.phtml', $data = null)
-//    {
-//        ob_start();
-//        include ROOT_PATH . '/application/views/content/' . $content;
-//        $content = ob_get_clean();
-//        include ROOT_PATH . '/application/views/layout/' . $layout;
-//
-//    }
-//
-//    function generateError($content, $layout = 'layout.phtml', $data = null)
-//    {
-//        ob_start();
-//        include ROOT_PATH . '/application/views/error/' . $content;
-//        $content = ob_get_clean();
-//        include ROOT_PATH . '/application/views/layout/' . $layout;
-//
-//    }
-
     private $_tpl;
     private $_layout;
     private $_data;
