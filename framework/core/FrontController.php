81f9703c9744d82a2e67a3716f2d2c938747f903
xbpcb <ch.kyrill@gmail.com>
2014-12-19 07:15:03 +0200

    Cloned structure + added EmailSender classes

--- framework/core/FrontController.php
+++ framework/core/FrontController.php
@@ -3,6 +3,7 @@
 include_once ROOT_PATH . '/framework/classes/Config.php';
 include_once ROOT_PATH . '/framework/classes/Registry.php';
 include_once ROOT_PATH . '/framework/classes/Database.php';
+include_once ROOT_PATH . '/framework/classes/EmailSender.php';
 include_once ROOT_PATH . '/framework/core/Model.php';
 include_once ROOT_PATH . '/framework/core/View.php';
 include_once ROOT_PATH . '/framework/core/Controller.php';
