ba61259c4deb8938b681c71c1ca65b85c10d3066
xbpcb <ch.kyrill@gmail.com>
2014-12-23 19:51:21 +0200

    Fixed class Paypal.php and added test-code to FrontController.php

--- framework/classes/Paypal.php
+++ framework/classes/Paypal.php
@@ -12,9 +12,9 @@ class Paypal {
      * @var array
      */
     protected $credentials = array(
-        'USER' => 'seller_1297608781_biz_api1.lionite.com',
-        'PWD' => '1297608792',
-        'SIGNATURE' => 'A3g66.FS3NAf4mkHn3BDQdpo6JD.ACcPc4wMrInvUEqO3Uapovity47p',
+        'USER' => 'ch.kyrill-facilitator_api1.gmail.com',
+        'PWD' => 'HTNT6R6EEH7Z76R6',
+        'SIGNATURE' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31AE.YSMq7OL6JtrdUhwzcO0-hJ0Az',
     );
 
     /**
@@ -29,7 +29,7 @@ class Paypal {
      * Версия API
      * @var string
      */
-    protected $version = '74.0';
+    protected $version = '98.0';
 
     /**
      * Сформировываем запрос
@@ -60,7 +60,7 @@ class Paypal {
             CURLOPT_VERBOSE => 1,
             CURLOPT_SSL_VERIFYPEER => true,
             CURLOPT_SSL_VERIFYHOST => 2,
-            CURLOPT_CAINFO => dirname(__FILE__) . '/cacert.pem', // Файл сертификата
+            CURLOPT_CAINFO => ROOT_PATH . '/cacert.pem', // Файл сертификата
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_POST => 1,
             CURLOPT_POSTFIELDS => $request
@@ -76,6 +76,7 @@ class Paypal {
         if (curl_errno($ch)) {
             $this -> errors = curl_error($ch);
             curl_close($ch);
+            var_dump($this->errors);
             return false;
         } else  {
             curl_close($ch);
@@ -85,3 +86,18 @@ class Paypal {
         }
     }
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/*The following cURL command makes an Express Checkout call to the Sandbox with my account data*/
+//curl -s --insecure https://api-3t.sandbox.paypal.com/nvp -d
+//"USER = ch.kyrill-facilitator_api1.gmail.com
+//PWD = HTNT6R6EEH7Z76R6
+//SIGNATURE = AFcWxV21C7fd0v3bYYYRCpSSRl31AE.YSMq7OL6JtrdUhwzcO0-hJ0Az
+//METHOD = SetExpressCheckout
+//VERSION = 98
+//PAYMENTREQUEST_0_AMT = 10
+//PAYMENTREQUEST_0_CURRENCYCODE = USD
+//PAYMENTREQUEST_0_PAYMENTACTION = SALE
+//cancelUrl = http://www.example.com/cancel.html
+//returnUrl = http://www.example.com/success.html"
--- framework/core/FrontController.php
+++ framework/core/FrontController.php
@@ -7,11 +7,17 @@ include_once ROOT_PATH . '/framework/classes/Database.php';
 include_once ROOT_PATH . '/framework/classes/Tools.php';
 include_once ROOT_PATH . '/framework/classes/ViewHelper.php';
 include_once ROOT_PATH . '/framework/classes/ChromePhp.php';
+include_once ROOT_PATH . '/framework/classes/Paypal.php';
 include_once ROOT_PATH . '/framework/core/Model.php';
 include_once ROOT_PATH . '/framework/core/View.php';
 include_once ROOT_PATH . '/framework/core/Controller.php';
 include_once ROOT_PATH . '/framework/core/Dispatcher.php';
 
-Config::init('dev');
-
-Dispatcher::start();
+//Config::init('dev');
+//
+//Dispatcher::start();
+$testRequest = new Paypal();
+echo '<hr />';
+foreach($testRequest->request('SetExpressCheckout') as $key => $value){
+    echo $key.': '.$value.'<hr />';
+};
