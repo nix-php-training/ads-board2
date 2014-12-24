7694de2cba032724dc439dd3c828ec3bb4dc028a
dimond-darrell <staskgt@gmail.com>
2014-12-24 08:38:47 +0200

    Merge branch 'master' of https://github.com/nix-php-training/ads-board2
    Conflicts:
    	framework/core/Dispatcher.php
    	index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-if (file_exists('gfun.php'))
-    include_once 'gfun.php';//help function
 
 ini_set('display_errors', 1);
 
-define("ROOT_PATH", dirname(__FILE__));
+define('ROOT_PATH', dirname(__FILE__));
 
 require_once ROOT_PATH . '/framework/core/FrontController.php';
