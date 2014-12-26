bdbf49597efe9e9588f06463d25b36989d55af1b
dchepiga <daria.chepiga@gmail.com>
2014-12-26 09:05:05 +0200

    [Remove] monolog

--- index.php
+++ index.php
@@ -6,5 +6,7 @@ if (file_exists('gfun.php')) {
 ini_set('display_errors', 1);
 
 define("ROOT_PATH", dirname(__FILE__));
+require 'vendor/autoload.php';
 
 require_once ROOT_PATH . '/framework/core/FrontController.php';
+
