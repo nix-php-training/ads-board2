0c699b4702a0deb0b9ffd4636b2a63ecab8d15c1
dchepiga <daria.chepiga@gmail.com>
2015-01-09 12:26:52 +0200

    [in progress]  working on getting Ads from DB

--- application/models/Advertisement.php
+++ application/models/Advertisement.php
@@ -4,9 +4,30 @@ class Advertisement extends Model
 {
     protected $table = 'advertisements';
 
-    function addAdvertisement($data)
+    public function addAdvertisement($data)
     {
-        $this->db->insert($this->table,$data);
+        $this->db->insert($this->table, $data);
+    }
+
+    public function getAllAdvertisements()
+    {
+        return $this->db->query('select a.subject, a.description, a.price, a.creationDate, c.title, u.login from advertisements a
+                                  INNER JOIN categories c on a.categoryId=c.id
+                                  INNER JOIN users u on a.userId=u.id')->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function splitCreationDate($ads)
+    {
+        foreach ($ads as $v)
+        {
+            var_dump($v['creationDate']);
+            $dateAndTime = explode( ' ', $v['creationDate']);
+            var_dump($dateAndTime);
+            $v['creationDate'] = $dateAndTime[0];
+            $v['creationTime'] = $dateAndTime[1];
+
+        }
+        return $ads;
     }
 
 }
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if (file_exists('gfun.php')) {
     include_once 'gfun.php';
 }//help function
 
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 
 define('ROOT_PATH', dirname(__FILE__));
 define('APP_ENV', getenv('APP_ENV'));
