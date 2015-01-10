0447308237f4fe465607e6c6eff921829ca4a4fe
diamond-darrell <staskgt@gmail.com>
2015-01-10 15:45:36 +0200

    [ADD] sort user array before render

--- application/assets/js/adminUsers.js
+++ application/assets/js/adminUsers.js
@@ -2,38 +2,22 @@ var app = angular.module('adminApp', ['smart-table']);
 
 app.controller('userCtrl', ['$scope', '$http', function ($scope, $http) {
 
-    //var testData = [
-    //    {
-    //        'login': 'dude',
-    //        'email': 'dude@mail.com',
-    //        'name': 'Olivier Renard',
-    //        'banned': false
-    //    },
-    //    {
-    //        'login': 'notDude',
-    //        'email': 'notdude@mail.com',
-    //        'name': 'Maria Frere',
-    //        'banned': false
-    //    },
-    //    {
-    //        'login': 'pepper',
-    //        'email': 'pepper@mail.com',
-    //        'name': 'Max Renard',
-    //        'banned': true
-    //    }
-    //];
-
     $scope.rowCollection = [];
     $scope.displayedCollection = [];
 
-    $http.get('/admin/showusers').success(function(data) {
-        $scope.rowCollection.push(data);
+    var byId = function (a, b) {
+        if (a.id < b.id)
+            return -1;
+        if (a.id > b.id)
+            return 1;
+        return 0;
+    };
 
-        console.log($scope.rowCollection);
-    }).error(function(){
+    $http.get('/admin/showusers').success(function (data) {
+        $scope.rowCollection = data.sort(byId);
+    }).error(function () {
 
     });
-
 
     $scope.displayedCollection = [].concat($scope.rowCollection);
 
