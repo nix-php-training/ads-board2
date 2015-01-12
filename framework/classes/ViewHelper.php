b5be1335dd84cf138e456fb865debd4039c5805f
dimond-darrell <staskgt@gmail.com>
2015-01-12 11:52:43 +0200

    [ADD] show error for login page (user not found), registration page (user with typed login/email exist)

--- framework/classes/ViewHelper.php
+++ framework/classes/ViewHelper.php
@@ -4,6 +4,14 @@
 trait ViewHelper
 {
 
+    /**
+     * Generate input with @var $attributes = array ('attrib' => 'value')
+     * with possibility assign value for html "value" attrib
+     *
+     * @param $attributes html attributes
+     * @param string $value html attribute "value="
+     * @return string generated input
+     */
     public function generateInput($attributes, $value = "")
     {
         $tag = '<input ';
