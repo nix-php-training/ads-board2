948de85c0d20ff61aa2f3c4612e1430299c53cc1
diamond-darrell <staskgt@gmail.com>
2015-01-17 11:38:50 +0200

    doesn't work yet

--- search.php
+++ search.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'framework/classes/sphinxapi.php';
+include 'framework/classes/ChromePhp.php';
 
 $s = new SphinxClient();
 $s->setServer("localhost", 3307);
@@ -8,14 +9,9 @@ $s->SetConnectTimeout(1);
 $s->SetArrayResult(true);
 $s->SetMatchMode(SPH_MATCH_ALL);
 
-$result1 = $s->query("stas");
-$result2 = $s->query("1");
+$result1 = $s->query("2015-01-13");
 
-//ChromePhp::log($result1);
+ChromePhp::log($result1);
 echo '<pre>';
 print_r($result1);
-echo '</pre>';
-
-echo '<pre>';
-print_r($result2);
 echo '</pre>';
