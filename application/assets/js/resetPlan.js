716506f70c960b2cc6a16e10058744e68b676d33
xbpcb <ch.kyrill@gmail.com>
2015-01-23 02:15:29 +0200

    Now dialog box works after user pressed RESET to free plan, fixed changePlan() within User.php - somehow it didnt work after merge

--- application/assets/js/resetPlan.js
+++ application/assets/js/resetPlan.js
@@ -1,11 +1,20 @@
-$(document).ready(function() {
-    $("#resetdialog").dialog({autoOpen:false,buttons:{
-        OK:function(){
-            $(this).dialog("close");
-            alert("ОК");},
-        CANCEL:function(){
-            $(this).dialog("close");
-            alert("CANCEL");}}
+$(function() {
+    $("#resetdialog").dialog({
+        autoOpen:false,
+        height: 200,
+        width: 500,
+        buttons:{
+            OK:function()
+            {
+                window.location.replace('/user/reset');
+                $(this).dialog("close");
+            },
+
+            CANCEL:function()
+            {
+                $(this).dialog("close");
+            }
+        }
     });
     $("#resetplan").click(function(){
         $("#resetdialog").dialog("open");
--- application/models/User.php
+++ application/models/User.php
@@ -238,7 +238,7 @@ class User extends Model
         $hash = $_COOKIE['hash'];
         $user = $this->getBy('hash', $hash);
         $this->db->query("UPDATE payments SET paymentType = 'paypal', endDate = DATE_ADD(NOW(), INTERVAL 1 MONTH ), price = '{$price}', planId = '{$planId}' WHERE userId = {$user['id']}");
-        $endDate = $this->db->fetchOne('payments','endDate',['userId' => $user->id]);
+        $endDate = $this->db->fetchOne('payments','endDate',['userId' => $user['id']]);
         $this->db->query("INSERT INTO operations(date,paymentType,planName,planCost,transactionId,userId) VALUES (DATE_ADD('{$endDate}', INTERVAL -1 MONTH),'paypal','{$planType}','{$price}','{$transactionId}',{$user['id']})");
     }
 
